ea24e2d62736acb6549e416e10bc6b151f0b8d30
SidraKlyara <72479505+SidraKlyara@users.noreply.github.com>
2023-04-14 18:51:03 -0600

    Did stuff

---
+++ globals.py
@@ -0,0 +1,18 @@
+
+def readfile(path):
+    with open(path) as file:
+        data = file.read()
+    return data
+
+def indent(text,levels=1,width=4,firstline=False):
+    indent = ' ' * width * levels
+    if firstline:
+        text = indent + text
+    return text.replace('\n', '\n' + indent)
+
+#BOILERPLATE=readfile("assets/icons/icons.html")+readfile('boilerplate.html')
+
+
+TS='type="text/typescript"'
+TSMODULE='type="tsmodule"'
+LESS='type="text/less"'
